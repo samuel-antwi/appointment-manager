248947e67534363c5ef80dbaabab186c37efc41a
Samuel Antwi <samuelantwidev@gmail.com>
2022-01-28 17:47:14 +0000

    save

--- src/supabase/index.js
+++ src/supabase/index.js
@@ -4,7 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 // const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL
-
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
