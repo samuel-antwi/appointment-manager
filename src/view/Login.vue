2aba37a9fba3ae47ca9f8791e9b3695d182d4003
Samuel Antwi <samuelantwidev@gmail.com>
2022-02-14 18:19:49 +0000

    implemented passwordless login

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -5,40 +5,24 @@
   import { useUser } from "../store/useUser"
 
   const email = ref("")
-  const password = ref("")
-  const showPassword = ref(false)
-  const passwordType = ref("password")
   let errorMsg = ref(null)
+  let statusMsg = ref(null)
   const router = useRouter()
 
   const userStore = useUser()
 
   const user = computed(() => userStore.user)
 
-  // Togglepassword type
-  const togglePasswordType = () => {
-    if (passwordType.value === "password") {
-      return (passwordType.value = "text")
-    }
-    return (passwordType.value = "password")
-  }
-
-  // Toggle show password
-  const toggleShowPassword = () => {
-    showPassword.value = !showPassword.value
-    togglePasswordType()
-  }
-
   const handleLogin = async () => {
     try {
-      const { user, error } = await supabase.auth.signIn({
+      const { error } = await supabase.auth.signIn({
         email: email.value,
-        password: password.value,
       })
       if (error) throw error
-      if (user) {
-        router.push({ name: "Home" })
-      }
+      statusMsg.value = `A login link has been sent to ${email?.value}`
+      setTimeout(() => {
+        statusMsg.value = null
+      }, 5000)
     } catch (error) {
       errorMsg.value = error.message
       setTimeout(() => {
@@ -60,7 +44,13 @@
     <div class="mx-auto max-w-2xl px-4">
       <!-- Error handling -->
       <div v-if="errorMsg" class="bg-light-grey relative mb-10 rounded-md p-4 shadow-lg">
-        <p class="text-red-500">{{ errorMsg }}</p>
+        <p class="text-red-500" v-if="errorMsg">{{ errorMsg }}</p>
+        <button class="absolute top-0 right-2 text-lg" @click="errorMsg = null">
+          &times;
+        </button>
+      </div>
+      <div v-if="statusMsg" class="bg-light-grey relative mb-10 rounded-md p-4 shadow-lg">
+        <p>{{ statusMsg }}</p>
         <button class="absolute top-0 right-2 text-lg" @click="errorMsg = null">
           &times;
         </button>
@@ -77,43 +67,12 @@
               type="email"
             />
           </div>
-          <div class="flex flex-col">
-            <label class="mb-1" for="email">Password*</label>
-            <div class="relative w-full">
-              <input
-                id="password"
-                v-model="password"
-                class="w-full px-2.5 py-3.5 focus:outline-none"
-                :type="passwordType"
-              />
-              <button
-                v-if="password"
-                type="button"
-                class="absolute top-2.5 right-5 text-gray-600"
-                @click="toggleShowPassword"
-              >
-                <fa v-if="showPassword" icon="eye" />
-                <fa v-else icon="eye-slash" />
-              </button>
-            </div>
-          </div>
           <button
-            class="bg-info px-10 py-3.5 font-medium uppercase tracking-wider text-gray-100 sm:self-start"
+            class="w-full bg-gray-900 px-10 py-3.5 font-semibold uppercase tracking-wider text-gray-100 hover:bg-gray-800"
             type="submit"
           >
-            Login
+            Send magic link
           </button>
-          <div class="items-center justify-between pt-3 sm:flex">
-            <router-link class="text-info" :to="{ name: 'ForgottenPassword' }"
-              >Forgotten password?</router-link
-            >
-            <p class="pt-3 text-gray-600 sm:pt-0">
-              Don't have an account yet?
-              <router-link :to="{ name: 'Signup' }" class="text-info"
-                >Sign up</router-link
-              >
-            </p>
-          </div>
         </form>
       </div>
     </div>
